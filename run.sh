2ef88482dde7466623d2dab215960ec47229aae7
Saulo Alves <sauloal@gmail.com>
2014-07-05 16:30:11 +0200

    agent working

--- run.sh
+++ run.sh
@@ -8,4 +8,11 @@
 #cd auto
   #docker build -t sauloal/shipyardauto .
 
-#docker run --privileged -d -v $PWD/data:/var/lib/docker -p 127.0.1.1:8000:8000 sauloal/shipyardauto
+docker run --privileged --name shipyard -d -v $PWD/data:/var/lib/docker -p 127.0.1.1:8000:8000 sauloal/shipyardauto
+#-p 127.0.1.1:4500:4500
+
+sleep 5
+
+SHIPIP=`docker inspect shipyard | grep IPAddress | perl -ne 'if( /(\d+\.\d+\.\d+\.\d+)/ ) { print $1; }'`
+
+docker run -d --name shipagent -v /var/run/docker.sock:/docker.sock -e IP=127.0.0.1 -e URL=http://${SHIPIP}:8000 -p 4500:4500 shipyard/agent
