2a1f9f1b58069979525c9431160a8ba75d9bc8a0
Saulo Alves <sauloal@gmail.com>
2014-09-10 22:53:58 +0200

    data in centralized folder

--- run.sh
+++ run.sh
@@ -8,7 +8,9 @@
 #cd auto
   #docker build -t sauloal/shipyardauto .
 
-docker run --privileged --name shipyard -d -v $PWD/data:/var/lib/docker -p 127.0.1.1:8000:8000 sauloal/shipyardauto
+SRC=$HOME/data/shipyard
+
+docker run --privileged --name shipyard -d -v $SRC:/var/lib/docker -p 127.0.1.1:8000:8000 sauloal/shipyardauto
 #-p 127.0.1.1:4500:4500
 
 sleep 5
